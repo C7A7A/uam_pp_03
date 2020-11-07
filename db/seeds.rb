4d9e134c8a768c445307d08f2fdd796d78c0f83c
C7A7A <56604258+C7A7A@users.noreply.github.com>
2020-11-07 21:47:46 +0100

    basic seeding (user + roles)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Role.delete_all
+roles = Role.create([
+  {
+    name: "admin"
+  },
+  {
+    name: "regular"
+  }
+])
+
+User.delete_all
+users = User.create([
+  {
+    name: "Mateusz",
+    surname: "Czajka",
+    date_of_birth: "09-07-1999",
+    login: "matcza",
+    password: "haslo123",
+    is_deleted: false,
+    role_id: roles.first.id
+  },
+  {
+    name: "Antoni",
+    surname: "Kolor",
+    date_of_birth: "12-01-2002",
+    login: "antkol",
+    password: "haslo123",
+    is_deleted: false,
+    role_id: roles.second.id
+  }
+])
